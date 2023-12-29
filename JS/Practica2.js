cdfdb4f4baaceab34d3b04b2f011ff445bfd9f4e
yaoska09 <yahoskaez@hotmail.com>
2023-12-29 00:08:38 -0600

    segunda solucion
    mejorando el codigo

---
+++ JS/Practica2.js
@@ -0,0 +1,76 @@
+
+
+function revision(){
+    let comprarRepuestos = true;
+    console.log("Revisando, espere por favor");
+    return comprarRepuestos;  
+}
+function Obtener_Info_Falla() {   
+    let decidir_Reparar = true;
+    console.log("obtener información de la falla");
+    return decidir_Reparar;        
+}
+function decidir_Garantia(tieneGarantia){
+    console.log("Revisando la Garantía");
+    if(tieneGarantia==true){
+        Obtener_Info_Falla();
+    }else{
+        console.log("Entregar celular")
+    }
+
+}
+function llamar_Cliente(){
+    console.log("LLamar al cliente");
+    decidir_Garantia();
+
+}
+function repara_Celular(){
+    console.log("Reparando Celular");
+    llamar_Cliente();
+}
+
+
+function comprar_Repuestos(){
+    console.log("comprando repuestos");
+    instalar_Repuestos();
+
+}
+function instalar_Repuestos(){
+    console.log("Instalando repuestos");
+    repara_Celular();
+
+}
+
+
+
+function decidir_Reparar_Celular (decidir_Reparar){
+    console.log("Diciendo reparar Celualar");
+    if(decidir_Reparar=true){
+        revision();
+    }else{
+        llamar_Cliente();
+
+    }
+    
+}
+
+function decidir_Repuestos(comprarRepuestos){
+    console.log("decidir si necesita repuestos");
+    if(comprarRepuestos=true){
+        comprar_Repuestos();
+    }else{
+        repara_Celular();
+
+    }
+
+}
+
+    
+
+
+
+
+function servicio_Celular(){
+    let info = Obtener_Info_Falla();
+    decidir_Reparar_Celular(info);
+}
